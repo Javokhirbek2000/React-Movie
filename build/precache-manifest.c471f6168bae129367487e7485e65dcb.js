159d3a0ff9c42c17847eef8061e6d5397cf62d70
Javokhirbek2000 <j.mirzaakbarov.2000@gmail.com>
2021-10-24 21:41:51 +0500

    Configured

---
+++ build/precache-manifest.c471f6168bae129367487e7485e65dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e8bfdb2a684488cba2b665c87475de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04b708b56a929d7489e",
+    "url": "/static/css/main.d8c9d474.chunk.css"
+  },
+  {
+    "revision": "f5a8a22cf5fe5632c841",
+    "url": "/static/js/2.1adcf764.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1adcf764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04b708b56a929d7489e",
+    "url": "/static/js/main.397c7f02.chunk.js"
+  },
+  {
+    "revision": "6b71224ab26933836c3b",
+    "url": "/static/js/runtime-main.9009ef15.js"
+  }
+]);
